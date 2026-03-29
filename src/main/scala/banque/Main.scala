a1b8679fee51805e4824a2bb1a940a25f36d7291
Ghoudi Souleim <sghoudi95@gmail.com>
2026-03-29 12:25:07 +0200

    Initialisation de la structure du projet Scala/Akka

---
+++ src/main/scala/banque/Main.scala
@@ -0,0 +1,8 @@
+package banque
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Système Bancaire Distribué - Groupe 5 - Initialisation OK")
+    // C'est ici que nous lancerons notre système d'acteurs Akka plus tard
+  }
+}
